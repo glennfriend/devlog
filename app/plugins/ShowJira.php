ed2d8f84162341787c0d8862b2065318e1fa147b
root <root@training3.training3>
2015-05-27 01:49:44 -0700

    add ShowJira plugin

---
+++ app/plugins/ShowJira.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ *  
+ */
+class ShowJira extends PluginBase
+{
+
+    private $desc = "show Jira link";
+
+    public function folder_view_header( $data )
+    {
+        $tags = $data['folder']->getProperty('tags');
+        foreach ( $tags as $tag ) {
+            if ( 'jira' != $tag['key'] ) {
+                continue;
+            }
+            $id  = $tag['value'];
+            $url = $this->getUrl($id);
+            $data['view']->content_header .= '<a href="'. $url .'" target="_blank">'. $id .'</a>';
+            break;
+        }
+    }
+
+    /**
+     *  
+     */
+    private function getUrl( $id )
+    {
+        return "https://simplybridal.atlassian.net/browse/" . $id;
+    }
+
+}
